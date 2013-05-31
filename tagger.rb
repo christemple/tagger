b828357f35318eb968d935c40312a85684dd0295
chris <temple3188@hotmail.co.uk>
2013-05-31 04:22:04 +0100

    Connecting to MongoDB and doing some simple queries

--- tagger.rb
+++ tagger.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
 require 'thin'
+require 'mongo'
+
+configure do
+  set :port, 3000
+end
+
+include Mongo
+
+$tagger = MongoClient.new.db('test').collection('tagger')
 
 get '/' do
-  'Hello World'
+  erb :index
 end
+
+post '/' do
+  @selected_tags = params['selected_tags']
+  @results = $tagger.find('tags' => {'$all' => @selected_tags})
+  erb :tags
+end
+
