9933696f294419ee95bc6e2af17f884e33cdcc3d
chris <temple3188@hotmail.co.uk>
2013-07-05 22:46:54 +0100

    Adding some chart love

--- tagger.rb
+++ tagger.rb
@@ -24,8 +24,24 @@ post '/' do
   @selected_tags = params['selected_tags'] || []
   @results = $tagger.aggregate([{'$match' => { tags: {'$all' => @selected_tags}, called_call_centre: true }},
                                 {'$group' => {'_id' => '$date_called', 'total_calls' => {'$sum' => 1}}},
-                                {'$sort' => { _id: -1 }}
+                                {'$sort' => { _id: 1 }}
                                ])
+  dates = @results.collect { |result| result["_id"] }
+  total_calls = @results.collect { |result| result["total_calls"] }
+  @chart_data = <<-JSON
+    {
+        "labels" : #{dates},
+        "datasets" : [
+            {
+                "fillColor" : "rgba(151,187,205,0.5)",
+                "strokeColor" : "rgba(151,187,205,1)",
+                "pointColor" : "rgba(151,187,205,1)",
+                "pointStrokeColor" : "#fff",
+                "data" : #{total_calls}
+            }
+        ]
+    }
+  JSON
   @total = @results.inject(0) {|total, result| total += result['total_calls'] }
   erb :index
 end
